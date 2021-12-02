0ac9fc8a0b1f6f2812d7fac598a73bca9e4f71cf
林承美 <91170746+choww26@users.noreply.github.com>
2021-12-02 09:47:46 +0800

    Add files via upload

--- week12/week12-4.cpp
+++ week12/week12-4.cpp
@@ -1,19 +1,20 @@
-int a[100];
+#include <stdio.h>
+int a[10]={3,0,1,8,7,2,5,4,6,9};
 int main()
 {
-	for(int i=0; i<100; i++){
-		scanf("%d",&a[i]);
-	}
-	for(int i=0; i<100; i++){
-		for(int j=i+1; j<100; j++){
-			if(a[i]>a[j]){
-				int temp=a[i];
-				a[i]=a[j];
-				a[j]=temp;
-			}
+    for(int i=0; i<10; i++)printf("%d ",a[i]);
+        printf("\n");
 
-		}
-	}
-	for(int i=0; i<100; i++)
-	printf("%d\n",a[i]);
+        for(int i=0; i<10; i++){
+            for(int j=i+1; j<10; j++){
+            if(a[i]>a[j]){
+                int temp= a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    for(int i=0; i<10; i++)
+        printf("%d ",a[i]);
+        printf("\n");
 }

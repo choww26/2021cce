9ea58f9e8c4e5534e5317b377a5b16469d3a6a4c
林承美 <91170746+choww26@users.noreply.github.com>
2021-12-22 11:58:15 +0800

    Add files via upload

---
+++ week15/week15-3.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d%d", &a, &b);
+    int c=a%b;
+
+    printf("a大的%d b中的%d c小的%d\n", a, b, c);
+    ///while(c!=0){
+    while(1){
+        if(c==0) break;///離開迴圈
+        a=b;
+        b=c;
+        c=a%b;
+        printf("a大的%d b中的%d c小的%d\n", a, b, c);
+    }
+    printf("因為c是0，離開迴圈，而且答案是中間的b\n", b);
+    printf("%d", b);
+}
+
